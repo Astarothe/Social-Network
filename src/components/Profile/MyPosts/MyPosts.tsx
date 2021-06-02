1985cbc12cc912dd1eb69aae87daa7733abffa3d
Astaroth <minatosavich@gmail.com>
2021-06-02 17:06:02 +0300

    feat: add map.message and map.myposts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,10 +4,12 @@ import {Post} from "./Post/Post";
 
 export function MyPosts() {
 
-    let postsData = [
+    let posts = [
         {id: 1, message: "Hi, how are you?", likesCount: 23},
         {id: 2, message: "It's my first post", likesCount: 0},
     ]
+
+    let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
     return (
         <div className={s.postsBlock}>
@@ -21,8 +23,7 @@ export function MyPosts() {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
-                <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
+                {postsElements}
             </div>
         </div>
     )
